e810297bdc95d3eb819097b0816e8a8310a7ee3e
Eduardo <israxx2@gmail.com>
2018-06-02 02:10:44 +0000

    error eventos solucionado

--- resources/views/estudiante/eventos.blade.php
+++ resources/views/estudiante/eventos.blade.php
@@ -4,20 +4,27 @@
 @section('eventos', 'active') 
 @section('content')
 
-    @foreach($eventos as $evento)
+    @if($eventos->isEmpty())
         <div class="card card-chart">
-            <div class="card-header bg-light">
-                    <h5 class="card-category">{{ $evento->modalidad->nombre }}</h5>
-                <h4 class="card-title">{{ $evento->nombre }}</h4>
-            </div>
-        <div class="card-body " >
-                    @if(empty($evento->descripcion))
-                        <p class= "text-muted">Sin descripción</p>
-                    @else
-                        <p class= "text-muted">{{ $evento->descripcion }}</p>
-                    @endif
+            <div class="card-body " >
+                <p class= "text-muted">No se han registrado eventos nuevos aún :(</p>
             </div>
         </div>
-    @endforeach
-
+    @else
+        @foreach($eventos as $evento)
+            <div class="card card-chart">
+                <div class="card-header bg-light">
+                        <h5 class="card-category">{{ $evento->modalidad->nombre }}</h5>
+                    <h4 class="card-title">{{ $evento->nombre }}</h4>
+                </div>
+            <div class="card-body " >
+                        @if(empty($evento->descripcion))
+                            <p class= "text-muted">Sin descripción</p>
+                        @else
+                            <p class= "text-muted">{{ $evento->descripcion }}</p>
+                        @endif
+                </div>
+            </div>
+        @endforeach
+    @endif
 @endsection
